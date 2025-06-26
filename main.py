a6957a4c7b92ae89a16454d1f2ff2a40107865fa
KRRISHKUMAR PATEL <74807595+krrpatel@users.noreply.github.com>
2025-06-26 06:56:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,26 +32,4 @@ if os.path.isfile(home_swarm_pem):
 
 # Step 6: Run sed command to edit YAML config
 #yaml_file = os.path.join(rl_swarm_dir, "hivemind_exp", "configs", "mac", "grpo-qwen-2.5-0.5b-deepseek-r1.yaml")
-sed_yaml_cmd = (
-    f"sed -i '"
-    f"s/^torch_dtype: .*/torch_dtype: float32/; "
-    f"s/^bf16: .*/bf16: false/; "
-    f"s/^tf32: .*/tf32: false/; "
-    f"s/^per_device_train_batch_size: .*/per_device_train_batch_size: 1/; "
-    f"s/^gradient_checkpointing: .*/gradient_checkpointing: false/"
-    f"' {yaml_file}"
-)
-##print("Running sed command to edit YAML config...")
-##subprocess.run(sed_yaml_cmd, shell=True)
-
-# Step 7: Insert ensure_bootstrap_success=False into grpo_runner.py
-##runner_file = os.path.join(rl_swarm_dir, "hivemind_exp", "runner", "grpo_runner.py")
-sed_runner_cmd = (
-    "sed -i 's/\\*\\*self\\._dht_kwargs(grpo_args)/"
-    "ensure_bootstrap_success=False, **self._dht_kwargs(grpo_args)/' "
-    + runner_file
-)
-#print("Running sed command to edit grpo_runner.py...")
-#subprocess.run(sed_runner_cmd, shell=True)
-
 print("All tasks completed successfully.")
